738587c7af0242c15892152729e65f47e249272f
Felipe Vogel <feoli7@hotmail.com>
2016-04-13 00:38:35 -0300

    Added files via upload

--- webWorker.js
+++ webWorker.js
@@ -1,4 +1,4 @@
-importScripts('http://feoli7.github.io/sobel.js');
+importScripts('/sobel.js');
  
 self.onmessage = function(event) {
   // Sobel constructor returns an Uint8ClampedArray with sobel data 
