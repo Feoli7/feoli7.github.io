9cf5ec1dd1b72c2432e59e301a20cd42b8cf1817
Felipe Vogel <feoli7@hotmail.com>
2016-06-17 01:46:37 -0300

    Add files via upload

--- WebWorker/ProcessarBLUE.js
+++ WebWorker/ProcessarBLUE.js
@@ -7,7 +7,7 @@ self.onmessage = function (e) {
     var l = e.data.length;
     var cdx = e.data.cdx;
 
-    RED(internoImg, l);
+    BLUE(internoImg, l);
 
     self.postMessage({ result: canvasData, cdx: cdx });
 };
