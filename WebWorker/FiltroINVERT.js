6f18708615123b95d008addd59a94a14ddfe3a75
Felipe Vogel <feoli7@hotmail.com>
2016-06-18 22:07:58 -0300

    Add files via upload

---
+++ WebWorker/FiltroINVERT.js
@@ -0,0 +1,8 @@
+﻿var INVERT =	function(internoImg, l) {
+
+    for(var i = 0; i < l; i += 4) {
+          internoImg[i] = 255 - internoImg[i];
+          internoImg[i + 1] = 255 - internoImg[i + 1];
+          internoImg[i + 2] = 255 - internoImg[i + 2];
+        }
+};
